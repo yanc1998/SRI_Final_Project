8003c56113f6743e449f0b5729cce3e067a5f197
yank1 <yancarloglez98@gmail.com>
2022-06-26 22:21:19 -0400

    front con expansion de consultas

--- frontend/src/components/find.vue
+++ frontend/src/components/find.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="container">
+    <div>
+      <b-form-select v-model="selected" :options="options"></b-form-select>
+      <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+    </div>
     <b-form>
       <b-form-input v-model="query" placeholder="Enter the file content"></b-form-input>
       <b-col lg="4" class="pb-2">
@@ -8,12 +12,25 @@
     </b-form>
     <div>
       <b-list-group>
-        <b-list-group-item button v-for="item in this.relevantDocuments" @click="download(item)">{{
+        <b-list-group-item button v-for="item in this.relevantDocuments" @click="download(item[1])">{{
+            item[1]
+          }}
+        </b-list-group-item>
+      </b-list-group>
+      <p>{{ text }}</p>
+    </div>
+
+    <div>
+      <p>Sugest</p>
+      <b-list-group>
+        <b-list-group-item button v-for="item in this.expand" @click="find_expand(item)">{{
             item
           }}
         </b-list-group-item>
       </b-list-group>
+      <p>{{ text }}</p>
     </div>
+
   </div>
 </template>
 
@@ -25,7 +42,14 @@ export default {
   data() {
     return {
       query: '',
-      relevantDocuments: []
+      text: '',
+      relevantDocuments: [],
+      expand: [],
+      selected: null,
+      options: [
+        {value: 'vectorial', text: 'Select the vectorial model'},
+        {value: 'booleano', text: 'Select the boolean model'},
+      ]
     }
   },
   methods: {
@@ -34,7 +58,7 @@ export default {
       const data = JSON.stringify({
         "file_name": name,
       });
-      
+
       const config = {
         method: 'post',
         url: 'http://127.0.0.1:5000/download',
@@ -46,17 +70,27 @@ export default {
 
       axios(config)
           .then((response) => {
+            this.text = JSON.stringify(response.data)
             console.log(JSON.stringify(response.data));
           })
           .catch(function (error) {
             console.log(error);
           });
     },
+    find_expand(query_expand) {
+      let newQuery = ''
+      for (const token of query_expand) {
+        newQuery += token + ' '
+      }
+      this.query = newQuery
+      this.find()
+    },
     find() {
 
       const data = JSON.stringify({
         "query": this.query,
-        "count": 5
+        "count": 5,
+        "type": this.selected
       });
 
       const config = {
@@ -71,7 +105,8 @@ export default {
       axios(config)
           .then((response) => {
             console.log(JSON.stringify(response.data["data"]));
-            this.relevantDocuments = response.data["data"]
+            this.relevantDocuments = response.data["data"][0]
+            this.expand = response.data["data"][1]
           })
           .catch(function (error) {
             console.log(error);
